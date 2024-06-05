9bf1372d490bfee2050484997452f1a59ba9a3eb
Richard Poelderl <richard.poelderl@gmail.com>
2024-06-05 18:43:25 +0200

    remove `cache()`

--- auth/config.ts
+++ auth/config.ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/prisma/client";
 
 const {
-    auth: uncachedAuth,
+    auth,
     handlers: { GET, POST },
     signIn,
     signOut,
@@ -16,14 +16,7 @@ const {
     session: { strategy: "jwt" },
   });
 
-    export const auth = cache(async () => {
-        try {
-          return await uncachedAuth();
-        } catch (err) {
-          console.error("Error fetching session", err);
-          // throw here instead of returning null so that nextjs navigation keeps working
-          // see https://github.com/vercel/next.js/discussions/64076
-          throw err;
-        }
-      });
-export { signIn, signOut, GET, POST };
+// export const auth = cache(async () => {
+//     return await uncachedAuth();
+// });
+export { signIn, signOut, GET, POST, auth };
